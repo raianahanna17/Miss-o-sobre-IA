020e3306462fb1ee5ff4cc9808fb792d057068d5
raianahanna17 <160878224+raianahanna17@users.noreply.github.com>
2024-06-23 18:29:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -45,8 +45,15 @@ function mostraAlternativas() {
   perguntaAtual = perguntas[atual];
 caixaPerguntas.textContent = perguntaAtual.enunciado;
   
-  function mostraAlternativas() {
+ function mostraAlternativa() {
 for (const alternativa of perguntaAtual.alternativas) {
 const botaoAlternativas = document. createElement(“button”);
-botaoAlternativas.textContent = alternativa;
+botaoAlternativas.textContent = alternativa.texto;
+botaoAlternativas.addEventListener(“click”,
+function () {
+atual++;
+mostraPergunta();
+});
 caixaAlternativas.appendChild(botaoAlternativas);
+}
+}
