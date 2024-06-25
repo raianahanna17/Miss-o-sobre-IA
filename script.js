5fb2fbb0350d4c12abb672f79f67e41cfc9bbdc5
raianahanna17 <160878224+raianahanna17@users.noreply.github.com>
2024-06-25 09:47:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,135 @@
-const caixaPrincipal = document.querySelector(‘.caixa-
-principal’);
-const caixaPerguntas = document.querySelector(‘.caixa-
-perguntas’);
-const caixaAlternativas = document.querySelector(‘.caixa-
-alternativas’);
-const caixaResultado = document.querySelector(‘.caixa-
-resultado’);
-const textoResultado = document.querySelector(‘.texto-
-resultado’);
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const perguntas = [
+{
+enunciado: "Qual o seu nível de conhecimento sobre o Reino Fungi ?",
+alternativas: [
+{
+texto: "Baixo",
 
-const perguntas = [ //abre lista de perguntas
-{ //abre objeto de pergunta
-enunciado: “Qual é o ”,
-alternativas:
-“A) Locais úmidos e quentes”,
-“B) Locais secos e frios”,
-“C) Locais ensolarados",
-“D) Locais ventilados"
-],
+afirmacao: "Você sabe apenas a base sobre o Reino Fungi "
 },
-  {
-enunciado: “2- Qual é a principal função dos fungos no ecossistema?”,
-alternativas: [
-“A) Realizar fotossíntese”,
-“B) Decompor matéria orgânica”,
-“C) Produzir oxigênio”,
-“D) Regular a temperatura do ambiente”
-],
+{
+texto: "Médio",
+
+afirmacao: "Você tem um bom entendimento sobre o Reino Fungi. "
 },
-  {
-enunciado: “3- Quais desses alimentos são mais propensos a serem contaminados por fungos?”,
+{
+texto: "Alto",
+
+afirmacao: "Você tem tem um conhecimento aprofundado sobre o Reino Fungi"
+}
+]
+},
+{
+enunciado: "1 - Qual dos seguintes ambientes é propício para o crescimento de fungos?",
 alternativas: [
-“A) Frutas frescas”,
-“B) Pães e bolos úmidos”,
-“C) Carnes frescas”,
-“D) Legumes secos”
-],
-}//fecha objeto de pergunta
-]; //fecha lista de perguntas
+{
+texto: "a)  Locais úmidos e quentes",
+
+afirmacao: " Esta alternativa esta correta pois os fungos se desenvolvem melhor em ambientes com alta umidade e temperatura, favorecendo seu crescimento e reprodução." 
+},
+{
+texto: "b) Locais secos e frios",
+
+afirmacao: " Está alternativa está incorreta. "
+},
+{
+texto: "c) Locais ensolarados",
+afirmacao: "Está alternativa está incorreta. "
+},
+{
+texto: "d) Locais ventilados",
+afirmacao: "Está alternativa está incorreta. "
+}
+
+]
+},
+{
+enunciado: "2 - Qual é a principal função dos fungos no ecossistema?",
+alternativas: [
+{
+texto: "a) Realizar fotossíntese",
+afirmacao: "Está alternativa está incorreta. "
+},
+{
+texto: "b) Decompor matéria orgânica",
+
+afirmacao: "Está alternativa está correta pois os fungos desempenham um papel fundamental na decomposição de matéria orgânica, contribuindo para o ciclo de nutrientes no ecossistema. "
+
+},
+{
+texto: "c) Produzir oxigênio",
+afirmacao: "Está altenativa está incorreta. "
+},
+{
+texto: "d) Regular a temperatura do ambiente",
+afirmacao: "Está alternativa está incorreta. "
+}
+]
+},
+{
+enunciado: "3 - Quais desses alimentos são mais propensos a serem contaminados por fungos ?",
+alternativas: [
+{
+texto: "a) Frutas frescas",
+afirmacao: "Está alternativa está incorreta. "
+},
+{
+texto: "b) Pães e bolos úmidos",
+afirmacao: "Está alternativa está correta pois alimentos ricos em umidade, como pães e bolos, são mais suscetíveis à contaminação por fungos devido às condições ideais para seu crescimento. "
+
+},
+{
+texto: "c) Carnes frescas",
+afirmacao: "Está alternativa está incorreta. "
+
+},
+
+{
+texto: "d) Legumes secos",
+afirmacao: "Está alternativa está incorreta. "
+
+}
+]
+}
+  
+
+];
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
+function mostraPergunta() {
+if (atual >= perguntas.length) {
+mostraResultado();
+return;
 }
-function mostraAlternativas() {
-  perguntaAtual = perguntas[atual];
+perguntaAtual = perguntas[atual];
 caixaPerguntas.textContent = perguntaAtual.enunciado;
-  
- function mostraAlternativa() {
-for (const alternativa of perguntaAtual.alternativas) {
-const botaoAlternativas = document. createElement(“button”);
+caixaAlternativas.textContent = "";
+mostraAlternativas();
+}
+function mostraAlternativas(){
+for(const alternativa of perguntaAtual.alternativas) {
+const botaoAlternativas = document.createElement("button");
 botaoAlternativas.textContent = alternativa.texto;
-botaoAlternativas.addEventListener(“click”,
-function () {
-atual++;
-mostraPergunta();
-});
+botaoAlternativas.addEventListener("click", () =>
+respostaSelecionada(alternativa));
 caixaAlternativas.appendChild(botaoAlternativas);
 }
 }
+function respostaSelecionada(opcaoSelecionada) {
+const afirmacoes = opcaoSelecionada.afirmacao;
+historiaFinal += afirmacoes + " ";
+atual++;
+mostraPergunta();
+}
+function mostraResultado() {
+caixaPerguntas.textContent = "Obrigado por completar o quiz!";
+textoResultado.textContent = historiaFinal;
+caixaAlternativas.textContent = "";
+}
+mostraPergunta();
